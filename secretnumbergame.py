956f1f04fdead088429c1e01f82bf714ddb34d2d
Ubuntu <ubuntu@ip-172-31-19-198.ec2.internal>
2019-10-01 18:37:37 +0000

    initial commit

---
+++ secretnumbergame.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Created by: Douglass Jeffrey
+# Created on: Sep 2019
+# This program plays a number generation game
+
+
+import constants
+
+
+def main():
+    # this function checks user inputted number is the same as the computers
+    # secret number
+
+    # input
+    user_number = int(input("Enter your number: "))
+    print("")
+
+    # process & output
+    if user_number == constants.SECRETNUMBER:
+        print("You got the answer right!!!")
+
+
+if __name__ == "__main__":
+    main()
